5bce8b91e16fb0176d29f5a9f48fe7241dac123b
kuldeep3 <kuldeepupreti3@gmail.com>
2019-09-05 02:33:44 +0530

    arithmetic operators

---
+++ ArithmeticOperators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1 = 240;
+    int num2 = 40;
+    cout<<"num1 + num2:"<<(num1 + num2)<<endl;
+    cout<<"num1 - num2:"<<(num1 - num2)<<endl;
+    cout<<"num1 * num2:"<<(num1 * num2)<<endl;
+    cout<<"num1 / num2:"<<(num1 / num2)<<endl;
+    cout<<"num1 % num2:"<<(num1 % num2)<<endl;
+    return 0;
+}
