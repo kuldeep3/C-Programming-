f55a019e77c989bfd982b205cca68e0ccfbd7974
kuldeep3 <kuldeepupreti3@gmail.com>
2019-09-05 12:18:13 +0530

    switch case

---
+++ switch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 2;
+    switch (i)
+    {
+    case 1:
+        cout<<"Case1 "<<endl;
+        break;
+    case 2:
+        cout<<"Case2 "<<endl;
+        break;
+    case 3:
+        cout<<"Case3 "<<endl;
+        break;
+    case 4:
+        cout<<"Case4 "<<endl;
+        break;
+    
+    default:
+        break;
+    }
+}
