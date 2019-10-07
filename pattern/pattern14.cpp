b9a6a7769db2dfb8dc196167f8dd723e622b98cb
kuldeep3 <kuldeepupreti3@gmail.com>
2019-10-07 21:36:31 +0530

    patterns

---
+++ pattern/pattern14.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++)
+    {
+        char c = 'A';
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<c;
+            c++;
+        }
+        cout<<endl;
+    }
+    
+}
